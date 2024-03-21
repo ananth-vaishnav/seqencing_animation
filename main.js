8277988e0b22a9ba2c2334f491281a547141df84
Anantha vaishnav A <152788361+ananth-vaishnav@users.noreply.github.com>
2024-03-21 22:28:54 +0530

    main.js

--- main.js
+++ main.js
@@ -1,50 +1,27 @@
 const aliceTumbling = [
-    { transform: 'rotate(0) scale(1)' },
-    { transform: 'rotate(360deg) scale(0)' }
+  { transform: 'rotate(0) scale(1)' },
+  { transform: 'rotate(360deg) scale(0)' }
 ];
 
 const aliceTiming = {
-    duration: 2000,
-    iterations: 1,
-    fill: 'forwards'
+  duration: 2000,
+  iterations: 1,
+  fill: 'forwards'
 }
 
 const alice1 = document.querySelector("#alice1");
 const alice2 = document.querySelector("#alice2");
 const alice3 = document.querySelector("#alice3");
 
-
-
-function doOperation1() {
-    alice1.animate(aliceTumbling, aliceTiming).finished.then(() => {
-        alice2.animate(aliceTumbling, aliceTiming).finished.then(() => {
-            alice3.animate(aliceTumbling, aliceTiming).finished.then(() => {
-                console.log("done");
-            });
-        });
-    });
+// Function to animate an element
+function animateElement(element) {
+  return element.animate(aliceTumbling, aliceTiming).finished;
 }
 
+// Sequence the animations
+animateElement(alice1).then(() => {
+  return animateElement(alice2);
+}).then(() => {
+  return animateElement(alice3);
+});
 
-
-function doOperation2() {
-    alice1.animate(aliceTumbling, aliceTiming).finished.then(() => {
-        return alice2.animate(aliceTumbling, aliceTiming).finished;
-    }).then(() => {
-        return alice3.animate(aliceTumbling, aliceTiming).finished;
-    }).then(() => {
-        console.log("done");
-    });
-
-}
-
-
-
-async function doOperation3() {
-    await alice1.animate(aliceTumbling, aliceTiming);
-    await alice2.animate(aliceTumbling, aliceTiming);
-    await alice3.animate(aliceTumbling, aliceTiming);
-    console.log("done");
-}
-
-doOperation2();
